527345814324e654c408cb40a140f70d8bf2ef4d
Subham Singh <subham.reek@gmail.com>
2025-08-12 01:52:38 +0200

    Add VSCode config and expand vector usage examples
    Added .vscode directory with launch, settings, and tasks for C++ development. Enhanced vectors.cpp with more detailed examples of iterators, range-based loops, and element access, and added a new example of iterating through a vector using indices.

--- Standard_Template_Library/vectors.cpp
+++ Standard_Template_Library/vectors.cpp
@@ -25,6 +25,9 @@ void explainvector()
         cout << v1[i] << " "; // Outputs the elements of vector v1.
     }
     cout << endl; 
+
+
+
     // iterators can also be used to traverse through the vector.
     //Iterators are used to point to the memory address of the elements in the vector.
     cout << "Using iterators: ";
@@ -37,6 +40,17 @@ void explainvector()
         cout << *it << " "; // Dereferencing the iterator to get the value it points to.
     }
     cout << endl;
+
+// Usage of iterator in a loop:
+for(vector<int>::iterator i=beginItr2;i<endItr2;i++)
+{
+    cout<<*i<<" "; // Here the * symbol is used to say that we want to print the value at the location pointed by the i iterator.
+
+    // If we wanted to print the adress of the location we would have used & instead.
+}
+
+
+
     // We can also use range-based for loop to iterate through the vector.
     cout << "Using range-based for loop: ";
     for (const auto& element : v1) {
@@ -44,13 +58,24 @@ void explainvector()
     }
     //foreach loop is also used to iterate through the vector.
     cout << endl;
+
+
+
+
     // We can also use the at() function to access elements at a specific index.
     cout << "Element at index 0: " << v1.at(0) << endl; // Outputs the first element of vector v1.
     cout << "Element at index 1: " << v1.at(1) << endl; // Outputs the second element of vector v1.
     cout << "Element at index 2: " << v1.at(2) << endl; // Outputs the third element of vector v1.
+
+
+
+
     // We can also use the front() and back() functions to access the first and last elements of the vector.
     cout << "First element: " << v1.front() << endl; // Outputs the first element of vector v1.
     cout << "Last element: " << v1.back() << endl; // Outputs the last element of vector v1.
+    
+    
+    
     // We can also remove elements from the vector using pop_back() function.
     v1.pop_back(); // Removes the last element of vector v1.
     cout << "v1 after pop_back: ";
@@ -58,18 +83,31 @@ void explainvector()
         cout << element << " "; // Outputs the elements of vector v1 after removing the last element.
     }
     cout << endl;
+
+
+
     // We can also clear the vector using clear() function.
     v1.clear(); // Clears all elements from vector v1.
     cout << "v1 size after clear: " << v1.size() << endl; // Outputs the size of vector v1 after clearing, which is 0.
+   
+   
+   
     // Vectors can also hold different types of values.
     vector<pair<int, string>> v4; // Declaration of a vector v4 that can hold pairs of integers and strings.
     v4.push_back(make_pair(1, "one")); // Adds a pair (1, "one") to vector v4.
     v4.push_back(make_pair(2, "two")); // Adds a pair (2, "two") to vector v4.
     cout << "v4 pairs: ";
-    for (const auto& p : v4) {
-        cout << "(" << p.first << ", " << p.second << ") "; // Outputs each pair in vector v4.
+    // Iterating through vectors
+    vector<int> vec={1,2,3,4,5,6};
+    for(int i = 0; i<vec.size();i++)
+    {
+        cout<<vec[i]<<" ";
+
     }
-    cout << endl;
+    cout<<endl;
+
+
+
 
 
 }
